230e8d9556e73473e13f8ea4511c1cd8333f3b11
Kamran-karman <139229920+Kamran-karman@users.noreply.github.com>
2023-07-23 22:50:25 +0300

    Update sposob.py

--- sposob.py
+++ sposob.py
@@ -14,18 +14,21 @@ class Molniay(arcade.Sprite):
         self.igrok = igrok
         self.en_x = 0
         self.en_y = 0
-        self.udar = True
+        self.udar = False
         self.s = 0
         self.s_kd = 300
 
     def update_animation(self, delta_time: float = 1 / 60):
+        self.radius.position = self.igrok.position
         self.s_kd += 1
+        if self.s_kd < 300:
+            self.udar = False
         if self.s <= 3 and self.udar and self.s_kd >= 300:
             spisok = []
             spisok2 = []
             spisok3 = []
             for sprite in self.sprite_list:
-                if self.radius.check_collision(sprite):
+                if self.radius.check_collision(sprite=sprite):
                     poz_x, poz_y = abs(sprite.center_x - self.igrok.center_x), \
                         abs(sprite.center_y - self.igrok.center_y)
                     pozi = (poz_x, poz_y)
@@ -51,7 +54,7 @@ class Molniay(arcade.Sprite):
                 radius.position = enx, eny
                 w = 0
                 while w < 4:
-                    if radius.check_collision(self.sprite_list):
+                    if radius.check_collision(sprite_list=self.sprite_list):
                         if self.igrok.position == (stx, sty):
                             pred_poz = 0, 0
                         else:
@@ -117,10 +120,51 @@ class Molniay(arcade.Sprite):
         if self.s > 3:
             self.udar = False
             self.s_kd = 0
-        if self.udar:
+        if self.udar and self.s_kd >= 300:
             self.s += 1
         elif not self.udar:
             self.s = 0
 
     def koordinati(self):
         return self.en_x, self.en_y
+
+
+class GnevTora(arcade.Sprite):
+    def __init__(self, sprite_list, pers):
+        super().__init__()
+        # Эта переменная указывает список целей
+        self.sprite_list = sprite_list
+
+        # Эта переменая - радиус поражения
+        self.radius = hit_box_and_radius.Radius()
+        self.pers = pers
+
+        # Счётчики
+        self.s2 = 0
+        self.s = 301
+
+        # Эта переменна указывает, поразило ли кого-нибудь
+        self.udar = False
+
+    def update_animation(self, delta_time: float = 1 / 60):
+        self.s += 1
+        if self.s <= 300:
+            self.udar = False
+        if self.udar and self.s2 < 3 and self.s > 300:
+            arcade.draw_circle_filled(self.pers.position[0], self.pers.position[1], 250, MOL_BLUE)
+            arcade.draw_circle_filled(self.pers.position[0], self.pers.position[1], 150, WHITE)
+
+        if self.s2 >= 3:
+            self.udar = False
+            self.s = 0
+        if self.udar:
+            self.s2 += 1
+        elif not self.udar:
+            self.s2 = 0
+
+    def on_update(self, delta_time: float = 1 / 60):
+        self.radius.position = self.pers.position
+
+        for sprite in self.sprite_list:
+            if arcade.check_for_collision(self.radius, sprite):
+                sprite.hp -= 20

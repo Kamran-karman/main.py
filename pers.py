1b103b3f11a516ae8f642fce66102555ce96d0f2
Kamran-karman <139229920+Kamran-karman@users.noreply.github.com>
2023-08-16 22:12:27 +0300

    Update pers.py

--- pers.py
+++ pers.py
@@ -84,6 +84,8 @@ class BetaMaster(arcade.Sprite):
     def __init__(self, sprite_list):
         super().__init__()
         self.hp = 1000
+        self.h = self.hp
+        self.smert = False
 
         self.center_x = KOOR_X
         self.center_y = KOOR_Y
@@ -118,6 +120,14 @@ class BetaMaster(arcade.Sprite):
         self.veter_otalk = sposob.VeterOtalkivanie(self, self.sprite_list)
 
     def pymunk_moved(self, physics_engine, dx, dy, d_angle):
+        if self.hp < 0:
+            self.smert = True
+        elif self.hp < self.h:
+            self.h = self.hp
+            print('igrok:', self.hp)
+        elif self.hp > self.h:
+            self.h = self.hp
+
         if dx < -D_ZONE and self.storona == 0:
             self.storona = 1
         elif dx > D_ZONE and self.storona == 1:
@@ -194,12 +204,14 @@ class Vrag(arcade.Sprite):
 
         self.sprite_list = sprite_list
         self.igrok = igrok
+        self.igrok_list = arcade.SpriteList()
+        self.igrok_list.append(igrok)
         self.v_drug_list = v_drug_list
 
         self.udar = False
         self.go = True
         self.d_zone = 40
-        self.drav = drav
+        self.s = 0
 
         self.kast_scena = kast_scena
 
@@ -221,6 +233,8 @@ class Vrag(arcade.Sprite):
         self.is_on_ground = True
         self.x_odometr = 0
 
+        self.mech = sposob.Mech(self, self.igrok_list, None, self.storona, 20)
+
     def pymunk_moved(self, physics_engine, dx, dy, d_angle):
         if not self.smert:
             if self.hp < 0:
@@ -239,6 +253,9 @@ class Vrag(arcade.Sprite):
                 self.storona = 0
 
             self.is_on_ground = physics_engine.is_on_ground(self)
+
+            if not self.go:
+                self.s += 1
 
             if arcade.check_for_collision(self.igrok, self.radius_vid) and not self.kast_scena:
 
@@ -277,24 +294,53 @@ class Vrag(arcade.Sprite):
                                     self.force_y = 0
 
                 for drug in self.v_drug_list:
-                    if (drug.center_x > self.center_x and abs(self.right - drug.left) <= 50 and not drug.go
-                            and self.igrok.center_x > self.center_x):
+                    if self.s <= 180:
+                        if (drug.center_x > self.center_x and abs(self.right - drug.left) <= 50 and not drug.go
+                                and self.igrok.center_x > self.center_x):
+                            self.go = False
+                            self.force_x, self.force_y = 0., 0.
+                            break
+                        elif (drug.center_x < self.center_x and abs(self.left - drug.right) <= 50 and not drug.go
+                              and self.igrok.center_x < self.center_x):
+                            self.go = False
+                            self.force_x, self.force_y = 0., 0.
+                            break
+                        elif (abs(self.right - drug.left) <= self.d_zone and drug.center_x > self.center_x
+                              and self.igrok.center_x > self.center_x):
+                            self.force_x = 0.
+                            break
+                        elif (abs(self.left - drug.right) <= self.d_zone and drug.center_x < self.center_x
+                              and self.igrok.center_x < self.center_x):
+                            self.force_x = 0.
+                            break
+                    elif (self.s > 180 and not self.go and arcade.check_for_collision(self.radius_prig, drug) and
+                          (drug.center_x > self.center_x or drug.center_x < self.center_x) and
+                          (self.igrok.center_x > self.center_x or self.igrok.center_x < self.center_x)):
                         self.go = False
-                        self.force_x, self.force_y = 0., 0.
-                        break
-                    elif (drug.center_x < self.center_x and abs(self.left - drug.right) <= 50 and not drug.go
-                            and self.igrok.center_x < self.center_x):
-                        self.go = False
-                        self.force_x, self.force_y = 0., 0.
-                        break
-                    elif (abs(self.right - drug.left) <= self.d_zone and drug.center_x > self.center_x
-                          and self.igrok.center_x > self.center_x):
-                        self.force_x = 0.
-                        break
-                    elif (abs(self.left - drug.right) <= self.d_zone and drug.center_x < self.center_x
-                          and self.igrok.center_x < self.center_x):
-                        self.force_x = 0.
-                        break
+                        if self.igrok.center_x > self.radius_vid.center_x:
+                            if abs(self.left - self.igrok.right) > self.d_zone:
+                                self.force_x = 15000.
+                            elif abs(self.left - self.igrok.right) <= self.d_zone:
+                                self.force_x = 0.
+                                self.go = False
+                                self.s = 0
+                                break
+
+                        elif self.igrok.center_x < self.radius_vid.center_x:
+                            if abs(self.right - self.igrok.left) > self.d_zone:
+                                self.force_x = -15000.
+                            elif abs(self.right - self.igrok.left) <= self.d_zone:
+                                self.force_x = 0.
+                                self.go = False
+                                self.s = 0
+                                break
+
+                        if (arcade.check_for_collision_with_list(self.radius_prig, self.sprite_list) and
+                                self.is_on_ground and abs(dx) < D_ZONE):
+                            if arcade.check_for_collision(self.radius_prig, drug):
+                                self.force_y = 50000.
+                            else:
+                                self.force_y = 0.
             else:
                 self.go = False
                 self.force_x, self.force_y = 0., 0.
@@ -320,9 +366,16 @@ class Vrag(arcade.Sprite):
                     self.sch_walk_tex = 0
                 self.texture = self.walk_t[self.sch_walk_tex][self.storona]
 
+    def on_update(self, delta_time: float = 1 / 60):
+        self.mech.storona = self.storona
+        if arcade.check_for_collision(self.radius_ataki, self.igrok):
+            self.mech.udar = True
+        else:
+            self.mech.udar = False
+        self.mech.on_update()
+
     def draw(self, *, filter=None, pixelated=None, blend_function=None):
-        if self.drav:
-            self.radius_prig.draw()
+        self.mech.draw()
 
     def update_animation(self, delta_time: float = 1 / 60):
         if self.smert:

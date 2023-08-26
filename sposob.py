461944f52e76d704a2e54e8efe1e90b208ae5d06
Kamran-karman <139229920+Kamran-karman@users.noreply.github.com>
2023-08-26 19:31:52 +0300

    Update sposob.py

--- sposob.py
+++ sposob.py
@@ -13,8 +13,22 @@ ORUZHIE = 1
 # ___Характеристики способностей___
 
 # __SharMolniay__
-SKOROST_SHAR_MOLNII = 5
+SKOROST_SHAR_MOLNII = 20
+URON_SHAR_MOL = 25
+URON1_SHAR_MOL = 5
+
 S_KD_SHAR_MOLNII = 300
+MAX_S_ZARYAD = 420
+MAX_BAF_S_ZARYAD = 300
+S_DO_PROMAH = 45
+
+VZRIV_BAF_URON = 1.5
+PROMAH_DEBAF_URON = 1.5
+BAF_URON = 19.067
+
+
+# __Shcit__
+URON_SHCHIT = 40
 
 
 # Стихия молнии
@@ -316,9 +330,12 @@ class StreliPeruna(arcade.Sprite):
             self.s = 0
 
 
-class SharMolniay(arcade.Sprite):
+class SharMolniay(arcade.Sprite): ######
     def __init__(self, pers, sprite_list):
         super().__init__()
+        self.uron = URON_SHAR_MOL
+        self.uron1 = URON1_SHAR_MOL
+
         self.sprite_list = sprite_list
         self.pers = pers
 
@@ -326,22 +343,36 @@ class SharMolniay(arcade.Sprite):
         self.texture = self.tex_shar[1]
         self.scale = 0.01
 
-        self.radius = hit_box_and_radius.Radius(self.scale)
-        self.radius1 = hit_box_and_radius.Radius(0.5)
+        self.radius = hit_box_and_radius.Radius()
+        self.radius1 = hit_box_and_radius.Radius()
 
         self.udar = False
         self.zaryad = False
+        self.zaryad_b = False
+        self.vzriv = False
+        self.promah = False
+        self.baf_uron = 1
 
-        self.s = 0
+        self.s_zaryad = 0
         self.s_kd = S_KD_SHAR_MOLNII
+        self.s = 0
+        self.s_change_x = 0
+        self.s1 = 0
+        self.atak = False
 
         self.slovar = {}
         self.s_slovar = 0
 
-        self.fizika = arcade.PhysicsEnginePlatformer(self)
-
     def on_update(self, delta_time: float = 1 / 60) -> None:
+        uron = self.uron
+
         self.s_kd += 1
+        self.radius.position = self.radius1.position = self.position
+        if self.s_kd < S_KD_SHAR_MOLNII:
+            self.zaryad = False
+
+        if self.change_x == 0:
+            self.position = self.pers.position
 
         if self.s_slovar == 0:
             self.s_slovar += 1
@@ -351,24 +382,121 @@ class SharMolniay(arcade.Sprite):
         if len(self.slovar) != len(self.sprite_list):
             self.s_slovar = 0
 
-        if self.s_kd < S_KD_SHAR_MOLNII:
-            self.zaryad = False
-
         if self.zaryad:
-            pass
+            self.udar = False
+            self.zaryad_b = True
+            self.s_zaryad += 1
+            if self.s_zaryad < MAX_BAF_S_ZARYAD:
+                self.scale += 0.05 / MAX_BAF_S_ZARYAD
+                self.baf_uron += BAF_URON / MAX_BAF_S_ZARYAD
 
-        if self.s > 5:
-            self.s = 0
+        if self.s_zaryad > MAX_S_ZARYAD:
+            self.vzriv = True
+            self.atak = True
+            self.pers.hp -= uron * VZRIV_BAF_URON * round(self.baf_uron, 2)
+            self.radius1.scale = self.scale * 1.5
+            for sprite in self.sprite_list:
+                if self.radius1.check_collision(sprite):
+                    self.udar = False
+                    for i in self.slovar:
+                        if sprite == i and not self.slovar[i]:
+                            self.slovar[i] = True
+                            sprite.hp -= uron * VZRIV_BAF_URON * round(self.baf_uron, 2)
+            self.s_zaryad = 0
             self.zaryad = False
             self.s_kd = 0
 
-        if self.zaryad:
+        if (self.udar and self.zaryad_b) or self.atak:
+            self.zaryad = False
             self.s += 1
-        else:
+            self.s_zaryad = 0
+            if self.s_change_x == 0:
+                self.s_kd = 0
+                self.s_change_x = 1
+                if self.pers.storona == 0:
+                    self.change_x = SKOROST_SHAR_MOLNII
+                else:
+                    self.change_x = -SKOROST_SHAR_MOLNII
+
+            if arcade.check_for_collision_with_list(self, self.sprite_list):
+                self.atak = True
+                self.radius1.scale = self.scale * 1.5
+                for sprite in self.sprite_list:
+                    if self.radius1.check_collision(sprite):
+                        self.udar = False
+                        for i in self.slovar:
+                            if sprite == i and not self.slovar[i]:
+                                self.slovar[i] = True
+                                sprite.hp -= uron * round(self.baf_uron, 2)
+
+            if self.s >= S_DO_PROMAH:
+                self.promah = True
+                self.udar = False
+                self.atak = True
+                for sprite in self.sprite_list:
+                    if self.radius1.check_collision(sprite):
+                        for i in self.slovar:
+                            if sprite == i and not self.slovar[i]:
+                                self.slovar[i] = True
+                                sprite.hp -= uron * round(self.baf_uron, 2) / PROMAH_DEBAF_URON
+
+        elif not self.udar and not self.atak:
+            self.s = 0
+            self.change_x = 0
             for i in self.slovar:
                 self.slovar[i] = False
+            if not self.zaryad:
+                self.baf_uron = 1
+                self.zaryad_b = False
+                self.scale = 0.01
+                self.s_change_x = 0
 
-        self.fizika.update()
+    def update_animation(self, delta_time: float = 1 / 60) -> None:
+        uron1 = self.uron1
+        if self.atak or self.zaryad or self.udar:
+            arcade.draw_circle_filled(self.center_x, self.center_y, 90, (44, 117, 255, 50), 5)
+        if self.atak:
+            self.s1 += 1
+            if self.s1 >= 3:
+                self.atak = False
+                self.promah = False
+                self.vzriv = False
+                self.s1 = 0
+            spisok_rast = []
+            spisok_xy = []
+            for sprite in self.sprite_list:
+                if self.radius.check_collision(sprite):
+                    rast = (abs(self.radius.center_x - sprite.center_x),
+                            abs(self.radius.center_y - sprite.center_y))
+                    xy = sprite.position
+                    spisok_rast.append(rast)
+                    spisok_xy.append(xy)
+
+            if len(spisok_rast) > 5:
+                while len(spisok_rast) > 5:
+                    max_index = spisok_rast.index(max(spisok_rast))
+                    spisok_rast.remove(spisok_rast[max_index])
+                    spisok_xy.remove(spisok_xy[max_index])
+
+            stx, sty = self.position
+            while len(spisok_rast) >= 1:
+                min_index = spisok_rast.index(min(spisok_rast))
+                enx, eny = spisok_xy[min_index]
+                arcade.draw_line(stx, sty, enx, eny, MOL_BLUE, 25)
+                arcade.draw_line(stx, sty, enx, eny, arcade.color.WHITE, 15)
+                for sprite in self.sprite_list:
+                    if sprite.position == (enx, eny):
+                        for i in self.slovar:
+                            if sprite == i and not self.slovar[i]:
+                                self.slovar[i] = True
+                                if self.vzriv:
+                                    sprite.hp -= uron1 * VZRIV_BAF_URON * round(self.baf_uron, 2)
+                                elif self.promah:
+                                    sprite.hp -= uron1 * round(self.baf_uron, 2) / PROMAH_DEBAF_URON
+                                else:
+                                    sprite.hp -= uron1 * round(self.baf_uron, 2)
+                spisok_rast.remove(spisok_rast[min_index])
+                spisok_xy.remove(spisok_xy[min_index])
 
 
 # Стихия ветра
@@ -388,10 +516,15 @@ class VeterOtalkivanie(arcade.Sprite):
         self.s = 0
         self.s1 = 300
 
+        self.force_x = 3000
+        self.force_y = 5000
+
         #self.rad = hit_box_and_radius.Radius(0.5)
         self.tex = arcade.load_texture_pair('nuzhno/radius_porazheniya.png')
         self.scale = 0.5
         self.texture = self.tex[1]
+
+        self.max = 5
 
     def on_update(self, delta_time: float = 1 / 60):
         if self.s3 == 0:
@@ -446,6 +579,39 @@ class VeterOtalkivanie(arcade.Sprite):
     def update_animation(self, delta_time: float = 1 / 60) -> None:
         self.texture = self.tex[self.igrok.storona]
 
+    def return_force(self, mass, xy: str):
+        force_x = self.force_x
+        force_y = self.force_y
+        if xy == 'x':
+            if mass < self.max:
+                procent_max = self.max / 100
+                procent_x = force_x / 100
+                force_x -= procent_x * (mass / procent_max)
+                if self.igrok.storona == 1:
+                    return force_x
+                else:
+                    return -force_x
+            else:
+                return 0
+        elif xy == 'y':
+            if mass < self.max:
+                procent_max = self.max / 100
+                procent_y = force_y / 100
+                force_y -= procent_y * (mass / procent_max)
+                return force_y
+            else:
+                return 0
+        elif xy == 'xy':
+            if mass < self.max:
+                procent_max = self.max / 100
+                procent_x = force_x / 100
+                procent_y = force_y / 100
+                force_x -= procent_x * (mass / procent_max)
+                force_y -= procent_y * (mass / procent_max)
+                return (force_x, force_y)
+            else:
+                return 0
+
 
 # Ближний юой
 class Mech(arcade.Sprite):
@@ -491,38 +657,9 @@ class Mech(arcade.Sprite):
                 if arcade.check_for_collision(self, sprite) and not sprite.block1:
                     for i in self.slovar:
                         if i == sprite and not self.slovar[i]:
-                            r = 10
-                            if sprite.reakciay < self.pers.reakciay:
-                                if sprite.reakciay * r <= self.pers.reakciay:
-                                    self.slovar[i] = True
-                                    sprite.hp -= self.uron
-                                else:
-                                    r -= 0.1
-                                    shanc = 100
-                                    while r > 0:
-                                        if sprite.reakciay * r <= self.pers.reakciay < sprite.reakciay * r + 0.1:
-                                            if random.randint(1, shanc) == 2:
-                                                self.slovar[i] = True
-                                                sprite.hp -= self.uron
-                                            else:
-                                                sprite.block = True
-                                        r -= 0.1
-                                        shanc -= 1
-                            elif sprite.reakciay > self.pers.reakciay:
-                                if self.pers.reakciay * r <= sprite.reakciay:
-                                    sprite.block = True
-                                else:
-                                    r -= 0.1
-                                    shanc = 100
-                                    while r > 0:
-                                        if self.pers.reakciay * r <= sprite.reakciay < self.pers.reakciay * r + 0.1:
-                                            if random.randint(1, shanc) == 2:
-                                                sprite.block = True
-                                            else:
-                                                self.slovar[i] = True
-                                                sprite.hp -= self.uron
-                                        r -= 0.1
-                                        shanc -= 1
+                            if not block_func(sprite, self.pers):
+                                self.slovar[i] = True
+                                sprite.hp -= self.uron
 
         if self.pers.storona == 0:
             self.left, self.bottom = self.pers.center_x, self.pers.center_y - 40
@@ -545,6 +682,8 @@ class Mech(arcade.Sprite):
 class Shchit(arcade.Sprite):
     def __init__(self, pers, sprite_list):
         super().__init__()
+        self.uron = URON_SHCHIT
+        
         self.pers = pers
         self.sprite_list = sprite_list
         self.scale = 0.5
@@ -596,8 +735,9 @@ class Shchit(arcade.Sprite):
                 if arcade.check_for_collision(sprite, self):
                     for i in self.slovar:
                         if i == sprite and not self.slovar[i]:
-                            self.slovar[i] = True
-                            sprite.hp -= 40
+                            if not block_func(sprite, self.pers):
+                                self.slovar[i] = True
+                                sprite.hp -= self.uron
             return
 
         if not self.udar:
@@ -622,8 +762,61 @@ class Shchit(arcade.Sprite):
 # Стихия земли
 
 
-class Block:
-    def __init__(self, pers):
-        pass
 
+def block_func(pers, sprite, force=None):
+    r = 10
+    if sprite.block1:
+        return True
+    if sprite.reakciay < pers.reakciay and force is None:
+        if sprite.reakciay * r <= pers.reakciay:
+            pers.block = True
+            return True
+        else:
+            r -= 0.1
+            shanc = 99
+            while r > 1:
+                if sprite.reakciay * (r + 0.1) < sprite.reakciay * r <= pers.reakciay:
+                    break
+                r -= 0.1
+                shanc -= 1
 
+            shanc1 = shanc
+            while shanc >= shanc1:
+                if random.randint(1, 100) == shanc:
+                    pers.block = True
+                    return True
+                shanc -= 1
+
+            return False
+    elif sprite.reakciay > pers.reakciay and force is None:
+        if pers.reakciay * r <= sprite.reakciay:
+            return False
+        else:
+            r -= 0.1
+            shanc = 99
+            while r > 1:
+                if sprite.reakciay * (r + 0.1) < sprite.reakciay * r <= pers.reakciay:
+                    break
+                r -= 0.1
+                shanc -= 1
+
+            shanc1 = shanc
+            while shanc >= shanc1:
+                if random.randint(1, 100) == shanc:
+                    return False
+                shanc -= 1
+
+            pers.block = True
+            return True
+    elif sprite.reakciay == pers.reakciay and force is None:
+        if random.randint(1, 2) == 1:
+            pers.block = True
+            return True
+        else:
+            return False
+
+    if force is not None:
+        if pers.storona == 0:
+            return force
+        else:
+            return -force

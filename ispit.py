11ff09b2fa3ffec3b6ea8c95c27fce20233312b9
Kamran-karman <139229920+Kamran-karman@users.noreply.github.com>
2023-09-24 21:04:03 +0300

    Update ispit.py

--- ispit.py
+++ ispit.py
@@ -1,12 +1,9 @@
 import random
-import time
 import arcade
 import arcade.gui
 from dataclasses import dataclass
-from array import array
 import arcade.gl
 import pers
-
 
 W = 1600
 H = 900
@@ -23,6 +20,9 @@ IGROK_JUMP_FORCE = 40000
 WALL_FRICTION = 0.8
 WALL_CT = 'wall'
 
+# Координаты игрока
+IGROK_POSITION = 0, 200
+
 
 @dataclass
 class Chasti:
@@ -34,18 +34,13 @@ class Chasti:
 class Igra1GlavaViev(arcade.View):
     def __init__(self):
         super().__init__()
-        arcade.set_background_color((255, 182, 193, 1))
-        self.f = False
-
-        # Переменные для частиц
-        self.chasti_list = []
-        self.program = self.window.ctx.load_program(vertex_shader='shederi_igra/ver_shad_tl_ogon.glsl',
-                                                    fragment_shader='shederi_igra/frag_shad_tl_ogon.glsl')
-        self.window.ctx.enable_only(self.window.ctx.BLEND)
+        arcade.set_background_color((255, 182, 193, 255))
 
         self.igrok = None
+        self.brend = None
 
         self.vrag_list = None
+        self.zhivie_vrag_list = None
         self.v_drug_list = None
 
         self.walls_list = None
@@ -65,12 +60,8 @@ class Igra1GlavaViev(arcade.View):
 
         self.s = 0
         self.s1 = 0
-        self.s_zoom = 0
-        self.s_zoom1 = 0
-        self.zoom = False
-        self.zoom1 = False
 
-        self.t_main_patch = (':resources:images/tiles/')
+        self.t_main_patch = ':resources:images/tiles/'
 
     def setup(self):
         self.kamera = arcade.Camera()
@@ -80,105 +71,109 @@ class Igra1GlavaViev(arcade.View):
 
         self.sprite_list = arcade.SpriteList()
         self.v_drug_list = arcade.SpriteList()
+        self.vrag_list = arcade.SpriteList()
 
         self.walls_list = arcade.SpriteList()
         for x in range(-10000, 10000, 128):
             wall = arcade.Sprite(f'{self.t_main_patch}grassMid.png')
             wall.position = x, 64
             self.walls_list.append(wall)
-            self.sprite_list.append(wall)
 
-        for x in range(192, 3000, 64):
-            wall = arcade.Sprite(f'{self.t_main_patch}grassMid.png', 0.5)
-            wall.position = x, 160
-            self.walls_list.append(wall)
-            self.sprite_list.append(wall)
-
-        for x in range(192, 3000, 64):
-            wall = arcade.Sprite(f'{self.t_main_patch}grassMid.png', 0.5)
-            wall.position = x, 224
-            self.walls_list.append(wall)
-            self.sprite_list.append(wall)
-
-        for x in range(-5000, -10, 384):
+        for x in range(-10000, 10000, 128):
             wall = arcade.Sprite(f'{self.t_main_patch}grassMid.png')
-            wall.position = x, 192
+            wall.position = x, -64
             self.walls_list.append(wall)
-            self.sprite_list.append(wall)
 
-        self.vrag_list = arcade.SpriteList()
+        for x in range(-10000, 10000, 128):
+            wall = arcade.Sprite(f'{self.t_main_patch}grassMid.png')
+            wall.position = x, -192
+            self.walls_list.append(wall)
 
-        self.igrok = pers.BetaMaster(self.vrag_list)
+        self.zhivie_vrag_list = arcade.SpriteList()
 
-        vrag = pers.Vrag(self.igrok, self.sprite_list, self.vrag_list)
-        vrag._position = 500, 400
-        self.vrag_list.append(vrag)
+        self.igrok = pers.Voyslav(self.vrag_list, self.fizika)
+        self.igrok.position = IGROK_POSITION
 
-        for x in range(700, 1400, 100):
-            vrag = pers.Vrag(self.igrok, self.sprite_list, self.vrag_list)
+        for x in range(200, 400, 100):
+            vrag = pers.ZhitelInnocentii(self.igrok, self.walls_list, self.zhivie_vrag_list, 204)
             vrag._position = x, 400
+            self.zhivie_vrag_list.append(vrag)
             self.vrag_list.append(vrag)
 
-        for vrag in self.vrag_list:
-            vrag.v_drug_list = self.vrag_list
+        for x in range(-400, -200, 100):
+            vrag = pers.ZhitelInnocentii(self.igrok, self.walls_list, self.zhivie_vrag_list, 204)
+            vrag._position = x, 400
+            self.zhivie_vrag_list.append(vrag)
+            self.vrag_list.append(vrag)
+
+        for x in range(700, 1000, 150):
+            voin_in = pers.VoinInnocentii(self.igrok, self.walls_list, self.v_drug_list, 202)
+            voin_in.position = x, 200
+            self.zhivie_vrag_list.append(voin_in)
+            self.vrag_list.append(voin_in)
+
+        vrag = pers.ZhitelInnocentii(self.igrok, self.walls_list, self.v_drug_list, 205)
+        vrag._position = -850, 300
+        self.zhivie_vrag_list.append(vrag)
+        self.vrag_list.append(vrag)
+
+        gromila = pers.Gromila(self.igrok, self.walls_list, self.v_drug_list)
+        gromila.position = -750, 300
+        self.zhivie_vrag_list.append(gromila)
+        self.vrag_list.append(gromila)
+
+        brend = pers.Brend(self.igrok, self.walls_list, self.v_drug_list)
+        brend._position = -3950, 200
+        self.zhivie_vrag_list.append(brend)
+        self.vrag_list.append(brend)
+
+        for vrag in self.zhivie_vrag_list:
+            vrag.v_drug_list = self.zhivie_vrag_list
 
         self.igrok.sprite_list = self.vrag_list
 
         self.fizika = arcade.PymunkPhysicsEngine(GRAVITY, DAMPING)
-        self.fizika.add_sprite(self.igrok, MASS_IGROK, 1, max_vertical_velocity=IG_MAX_VERTICAL_SPEED,
+        self.fizika.add_sprite(self.igrok, MASS_IGROK, FRICTION_IGROK, max_vertical_velocity=IG_MAX_VERTICAL_SPEED,
                                max_horizontal_velocity=IG_MAX_HORIZANTAL_SPEED,
-                               moment_of_inertia=arcade.PymunkPhysicsEngine.MOMENT_INF, damping=0.9, collision_type='player')
-        self.fizika.add_sprite_list(self.walls_list, friction=1,
-                                    body_type=arcade.PymunkPhysicsEngine.STATIC, collision_type='wall')
-        for vrag in self.vrag_list:
+                               moment_of_inertia=arcade.PymunkPhysicsEngine.MOMENT_INF, damping=0.9)
+        self.fizika.add_sprite_list(self.walls_list, friction=WALL_FRICTION, collision_type=WALL_CT,
+                                    body_type=arcade.PymunkPhysicsEngine.STATIC)
+
+        for vrag in self.zhivie_vrag_list:
             self.fizika.add_sprite(vrag, 2, 1, max_vertical_velocity=IG_MAX_VERTICAL_SPEED,
                                    max_horizontal_velocity=200,
                                    moment_of_inertia=arcade.PymunkPhysicsEngine.MOMENT_INF, damping=0.9)
 
+        self.igrok.fizika = self.fizika
+
     def on_draw(self):
-        self.kamera.use()
-        self.center_kamera_za_igrok()
         self.clear()
 
         self.smert_list1.draw()
-        self.vrag_list.draw()
+        for vrag in self.zhivie_vrag_list:
+            vrag.draw()
+            vrag.update_animation()
         self.igrok.draw()
+        self.igrok.update_animation()
         self.smert_list2.draw()
 
         self.walls_list.draw()
-        for vrag in self.vrag_list:
-            vrag.draw()
-            vrag.update_animation()
 
-        self.igrok.update_animation()
+        self.kamera.use()
+        self.center_kamera_za_igrok()
 
     def on_update(self, delta_time: float):
-        if self.zoom:
-            self.s_zoom += 1
-            if self.s_zoom > 180:
-                self.s_zoom = 0
-                self.zoom = False
-            if self.zoom:
-                if self.s_zoom <= 180:
-                    self.kamera.zoom -= 1 / 1800
-        if self.zoom1:
-            self.s_zoom1 += 1
-            if self.s_zoom1 > 180:
-                self.s_zoom1 = 0
-                self.zoom1 = False
-            if self.zoom1:
-                if self.s_zoom1 <= 180:
-                    self.kamera.zoom += 1 / 1800
-
         self.igrok.on_update()
-        for vrag in self.vrag_list:
+
+        for vrag in self.zhivie_vrag_list:
             if vrag.smert:
                 if random.randint(0, 1) == 1:
                     self.smert_list1.append(vrag)
-                elif random.randint(0, 1) == 0:
+                else:
                     self.smert_list2.append(vrag)
-                self.vrag_list.remove(vrag)
+                self.zhivie_vrag_list.remove(vrag)
                 self.fizika.remove_sprite(vrag)
+
             else:
                 vrag.on_update()
 
@@ -189,11 +184,6 @@ class Igra1GlavaViev(arcade.View):
                     self.s += 1
                 if self.s > 2 and not vrag.is_on_ground:
                     y = 0
-
-                if self.igrok.veter_otalk.udar:
-                    x -= self.igrok.veter_otalk.return_force(2, 'x')
-                    #y -= self.igrok.veter_otalk.return_force(2, 'y')
-                    print(x, y)
 
                 if abs(x) > 0:
                     self.fizika.apply_force(vrag, (x, y))
@@ -206,19 +196,22 @@ class Igra1GlavaViev(arcade.View):
                 if y == 0 and self.s >= 2:
                     self.s = 0
 
-        if not self.f:
-            if self.pravo and not self.levo:
-                force = (IGROK_MOVE_GROUND, 0)
-                self.fizika.apply_force(self.igrok, force)
-                self.fizika.set_friction(self.igrok, 0.1)
-            elif not self.pravo and self.levo:
-                force = (-IGROK_MOVE_GROUND, 0)
-                self.fizika.apply_force(self.igrok, force)
-                self.fizika.set_friction(self.igrok, 0.1)
-            else:
-                self.fizika.set_friction(self.igrok, 1)
+                if vrag.return_position:
+                    self.fizika.set_position(vrag, vrag.return_position_func())
 
-        if self.igrok.molniya.action and self.s1 == 0 and self.igrok.molniya.s >= self.igrok.molniya.timer_for_s:
+        if self.pravo and not self.levo:
+            force = (self.igrok.mor_func(IGROK_MOVE_GROUND), 0)
+
+            self.fizika.apply_force(self.igrok, force)
+            self.fizika.set_friction(self.igrok, 0.5)
+        elif not self.pravo and self.levo:
+            force = (self.igrok.mor_func(-IGROK_MOVE_GROUND), 0)
+            self.fizika.apply_force(self.igrok, force)
+            self.fizika.set_friction(self.igrok, 0.5)
+        else:
+            self.fizika.set_friction(self.igrok, 1)
+
+        if self.igrok.molniya.tp and self.s1 == 0:
             self.s1 += 1
             poz = self.igrok.molniya.return_position()
             self.fizika.set_position(self.igrok, poz)
@@ -226,25 +219,17 @@ class Igra1GlavaViev(arcade.View):
         self.fizika.step()
 
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.L:
-            self.igrok.kulak_gaia.udar = True
+        if symbol == arcade.key.RALT:
+            self.igrok.udar_Zevsa.action = True
 
         if symbol == arcade.key.RCTRL:
-            self.igrok.veter_otalk.udar = True
+            self.igrok.shar_mol.zaryad = True
 
         if symbol == arcade.key.RSHIFT:
             self.igrok.block.block = True
 
-        if symbol == arcade.key.LCTRL:
-            self.zoom1 = True
-            self.zoom = False
-
-        if symbol == arcade.key.Z:
-            self.zoom = True
-            self.zoom1 = False
-
         if symbol == arcade.key.SPACE:
-            self.igrok.udar.action = True
+            self.igrok.shchit.action = True
 
         if symbol == arcade.key.NUM_2:
             self.igrok.streliPeruna.action = True
@@ -266,11 +251,14 @@ class Igra1GlavaViev(arcade.View):
                 force = (0, IGROK_JUMP_FORCE)
                 self.fizika.apply_force(self.igrok, force)
 
-        if symbol == arcade.key.F:
-            self.f = True
-            self.fizika.remove_sprite(self.igrok)
-
     def on_key_release(self, _symbol: int, _modifiers: int):
+        if _symbol == arcade.key.RALT:
+            self.igrok.udar_Zevsa.action = False
+
+        if _symbol == arcade.key.RCTRL:
+            self.igrok.shar_mol.action = True
+            self.igrok.shar_mol.zaryad = False
+
         if _symbol == arcade.key.RSHIFT:
             self.igrok.block.block = False
 
@@ -278,12 +266,6 @@ class Igra1GlavaViev(arcade.View):
             self.pravo = False
         elif _symbol == arcade.key.A or _symbol == arcade.key.LEFT:
             self.levo = False
-
-        if _symbol == arcade.key.G:
-            self.f = False
-            self.fizika.add_sprite(self.igrok, MASS_IGROK, FRICTION_IGROK, max_vertical_velocity=IG_MAX_VERTICAL_SPEED,
-                                   max_horizontal_velocity=IG_MAX_HORIZANTAL_SPEED,
-                                   moment=arcade.PymunkPhysicsEngine.MOMENT_INF, damping=0.9, collision_type='player')
 
     def center_kamera_za_igrok(self, x=False, y=False):
         ekran_center_x = self.igrok.center_x - self.kamera.viewport_width / 3
